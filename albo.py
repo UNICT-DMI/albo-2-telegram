db6fddfb4442dbd84b9ae55f5087a965c810856c
LukeTheWalker <32610222+LukeTheWalker@users.noreply.github.com>
2022-04-27 00:42:05 +0200

    feat: added bot-driven exception forwarding

--- albo.py
+++ albo.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlencode, quote_plus
+from urllib.parse import unquote, urlencode, quote_plus
 from typing import List
 import json
+import re
 
 TOKEN = ""
 CHATID = 123456
+CHAT_ID_DEV = 123456
 
 def escape_char (text: str, char_to_escape: List[str] = ['_', '*', '[', '`']) -> str:
   for char in char_to_escape:
@@ -19,7 +21,7 @@ def send_https_request (URL: str) -> None:
     raise ValueError (json.dumps(responseJSON, indent=2), URL.split("&")[-1])
 
 
-def send_telegram_message(text: str, chat_id = CHATID) -> None:
+def send_telegram_message(text: str, chat_id: int = CHATID) -> None:
     params = {
         'chat_id': chat_id,
         'parse_mode': 'markdown',
@@ -28,19 +30,19 @@ def send_telegram_message(text: str, chat_id = CHATID) -> None:
     URL = "https://api.telegram.org/bot" + TOKEN + "/sendMessage?&" + urlencode(params, quote_via=quote_plus)
     send_https_request (URL)
 
-def send_single_telegram_attachment(pdf_link: str) -> None:
+def send_single_telegram_attachment(pdf_link: str, chat_id: int = CHATID) -> None:
     params = {
-        'chat_id': CHATID,
+        'chat_id': chat_id,
         'disable_notification': True,
         'document': pdf_link
     }
     URL = "https://api.telegram.org/bot" + TOKEN + "/sendDocument?&" + urlencode(params, quote_via=quote_plus)
     send_https_request (URL)
 
-def send_multiple_telegram_attachments(pdf_links: List[str]) -> None:
+def send_multiple_telegram_attachments(pdf_links: List[str], chat_id: int = CHATID) -> None:
     input_media_documents = [ {   "type": "document",  "media": pdf_link   }  for pdf_link in pdf_links]
     params = {
-        'chat_id': CHATID,
+        'chat_id': chat_id,
         'disable_notification': True,
         'media': json.dumps(input_media_documents),
     }
@@ -85,8 +87,9 @@ for id in range (last_id + 1, new_id + 1):
     attachments = ["http://albo.unict.it/" + list_item['href'] for list_item in tr.find_all('a')]
     send_telegram_attachments(attachments)
   except ValueError as err:
-    error_string = "response sent: " + err.args[0] + "\n" + "last parameter: "+ err.args[1]
-    print (error_string) #Insert here bot sending error message in group
+    error_string = "response sent: " + err.args[0] + "\n" + "related documents:\n" + '\n'.join(re.findall(r"https?:.+?(?=\\)", json.dumps(unquote(err.args[1])), re.MULTILINE))
+    #print (error_string) #Insert here bot sending error message in group
+    send_telegram_message(escape_char(error_string), CHAT_ID_DEV)
   #print(message)
 
 with open("last_id.txt", "w+") as f:
